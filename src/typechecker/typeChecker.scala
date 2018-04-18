261a9e6d9cf2ec361223fc55e08159efc4124fb8
Ayushi Vipul Patel <ayushi27@uab.edu>
2018-04-17 22:57:43 -0500

    Working. Need to clean up.

--- src/typechecker/typeChecker.scala
+++ src/typechecker/typeChecker.scala
@@ -1,63 +1,110 @@
-package typechecker
-
 import scala.io.Source
 import scala.collection.mutable.HashMap
 
-class typeChecker(lines: List[String], env: List[String]){
-    var envRule = env.map(str => {
-        var spl = str.replaceAll("\\s","").replaceAll("&","").split('(')
-        val operator = spl(0).trim()
-        spl = spl(1).split(')')
-        var temp = spl(0).split(',')
-        val parameter = temp.toList
-        spl = spl(1).split('>')
-        val result = spl(1).trim()
-        ops(operator, parameter, result)
-    })
-    
-    var linesRule = lines.map(str => {
-        var spl = str.replaceAll("\\s","").replaceAll("&","").split('(')
-        val operator = spl(0).trim()
-        spl = spl(1).split(')')
-        var temp = spl(0).split(',')
-        val parameter = temp.toList
-        spl = spl(1).split('>')
-        val result = spl(1).trim()
-        ops(operator, parameter, result)
-    })
-    
-    var hash = collection.mutable.Map[String, String]()
-    
-    while(!linesRule.isEmpty){
-        var item = linesRule.head
-        var newlist = envRule.filter(x => x.operator == item.operator)
-        newlist = newlist.filter(x => x.parameters.size == item.parameters.size)
-        println(newlist)
-        println(item)
-        linesRule = linesRule.tail
-    }
-    
-    println("All operations type matched")
-    System.exit(0)
+class typeChecker(expressionLines: List[String], environmentLines: List[String]){
+	var envRule = environmentLines.map(str => {
+		var spl = str.replaceAll("\\s","").replaceAll("&","").split('(')
+				val operator = spl(0).trim()
+				spl = spl(1).split(')')
+
+				var temp = spl(0).split(',')
+				val parameter = temp.toList
+
+				spl = spl(1).split('>')
+				val result = spl(1).trim()
+
+				ops(operator, parameter, result)
+	})
+
+			var linesRule = expressionLines.map(str => {
+				var spl = str.replaceAll("\\s","").replaceAll("&","").split('(')
+						val operator = spl(0).trim()
+						spl = spl(1).split(')')
+
+						var temp = spl(0).split(',')
+						val parameter = temp.toList
+
+						spl = spl(1).split('>')
+						val result = spl(1).trim()
+
+						ops(operator, parameter, result)
+			})
+
+			var hash = collection.mutable.Map[String, String]()
+
+			// head takes the first element
+			// tail takes the rest
+			while(!linesRule.isEmpty){
+				var item = linesRule.head
+						var newlist = envRule.filter(x => x.operator == item.operator)
+						newlist = newlist.filter(x => x.parameters.size == item.parameters.size)
+
+						var size = item.parameters.size - 1
+						var index = 0
+						for(index <- 0 to size){
+							var value = item.parameters(index)
+									if (value.charAt(0) == '$') {
+										if (hash.contains(value)) {
+											var hashedvalue = hash(value)
+													if (hashedvalue == newlist.head.parameters(index)){ 
+														println("Already in map")
+													}
+													else {
+														println("FAILURE.")
+														System.exit(1)
+													}
+										}
+										else {
+											hash.put(value, newlist.head.parameters(index))
+											println("inserted into map")
+										}
+									}
+						}
+				var results = item.result
+						if (hash.contains(results)) {
+							var hashedvalue = hash(results)
+									if (hashedvalue == newlist.head.result){
+										println("Already in map")
+									}
+									else {
+										println("results: " + results)
+										println("hashedvalue: " + hashedvalue)
+										println("FAILURE.")
+										System.exit(1)
+									}
+						}
+						else {
+							hash.put(results, newlist.head.result)
+							println("inserted into map")
+						}
+
+				println(newlist)
+				println(item)        
+				println()
+
+
+				linesRule = linesRule.tail
+			}
+
+	println("All operations type matched")
+	System.exit(0)
 }
 
 object Main{
-    def main(args: Array[String]): Unit = {
-        val filename = scala.io.Source.fromFile(args(0))
-        val fn = scala.io.Source.fromFile(args(1))
-        
-        val env = fn.getLines.toList
-        val lines = filename.getLines.toList
-        
-        val tc = new typeChecker(lines, env)
-    }
+	def main(args: Array[String]): Unit = {
+			val filename = scala.io.Source.fromFile(args(0)) // Expression text file
+
+					val fn = scala.io.Source.fromFile(args(1)) // Environment Text file input
+					val environmentLines = fn.getLines.toList
+
+					val expressionLines = filename.getLines.toList
+
+					val tc = new typeChecker(expressionLines, environmentLines)
+	}
 }
 
-case class ops(operator:String, parameters:List[String], result: String){}
+// Creates a case class for different parameters needed for the operations
+case class ops(operator:String, parameters:List[String], result: String){
 
-// head takes the first element
-// tail takes the rest
 
-// to compile scalac test.scale
-// to execute scala test.scala input.txt env.txt
-// this will compile and run it at the same time
+}
